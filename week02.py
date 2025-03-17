4773d58fcaecc9464a34c44a30a45d4583212a52
Lcsvictory <yuko135z@gmail.com>
2025-03-17 19:36:07 +0900

    O(log n) guess game

--- week02.py
+++ week02.py
@@ -1,4 +1,34 @@
-# shift + F10
-n = int(input("정수 입력 : "))
-result = n * (n + 1) // 2
-print(f"1부터 n까지 누적합 = {result}")
+import random
+import math
+
+n = 100
+random_number = random.randint(1, n)
+print(random_number)
+win = False
+count = 0
+chance = int(math.log(n,2)) + 1
+
+print(10*"*"+"guess game!"+10*"*")
+while not win :
+    if count >= chance:
+        break
+    user_input = int(input(f"[남은기회{chance}]1~{n}사이의 숫자를 입력하세요 : "))
+    if user_input == random_number:
+        win = True
+        print(f"당신이 맞췄습니다. 정답 : {random_number}")
+    elif user_input > random_number:
+        count += 1
+        print(f"정답보다 큽니다. 질문횟수 : {count}")
+    elif user_input < random_number:
+        count += 1
+        print(f"정답보다 작습니다. 질문횟수 : {count}")
+    else:
+        print("잘못 입력하셨습니다.")
+
+if win:
+    print("축하합니다.")
+else:
+    print("안타깝습니다.")
+
+
+
