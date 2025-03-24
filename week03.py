b64f0bd6661c404aecc00ec6510d365274d7419e
Lcsvictory <yuko135z@gmail.com>
2025-03-24 17:39:00 +0900

    zip method

--- week03.py
+++ week03.py
@@ -1,17 +1,5 @@
-def move_zeros(l):
-    zero_index = 0 #0 자리를 알려주는 변수구나.
-    for i,n in enumerate(l):
-        if n != 0:
-            l[zero_index] = n
-            if zero_index != i: #제자리 바꿔치기가 아니라면.
-                l[i] = 0
-            zero_index += 1
-    return(l)
+gpus = ["RTX4090", "RTX5090", "H100", "RTX3090ti"]
+prices = [200, 500, 3000, 100]
 
-temp_list = [23,44,0,-4123,312,0,222,0,321]
-#temp_list = [23,44,-4123,0,312,0,222,0,321]
-#temp_list = [23,44,-4123,312,222,0,0,321,0]
-
-temp_list_move_zeros = move_zeros(temp_list)
-print(temp_list)
-print(temp_list_move_zeros)
+gpu = list(zip(gpus, prices))
+print(gpu)
