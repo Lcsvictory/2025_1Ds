c2b66e52060c85b2e04e541dd48c37794203c223
Lcsvictory <yuko135z@gmail.com>
2025-03-24 17:29:11 +0900

    zeros move

--- week03.py
+++ week03.py
@@ -1,6 +1,17 @@
-import array
+def move_zeros(l):
+    zero_index = 0 #0 자리를 알려주는 변수구나.
+    for i,n in enumerate(l):
+        if n != 0:
+            l[zero_index] = n
+            if zero_index != i: #제자리 바꿔치기가 아니라면.
+                l[i] = 0
+            zero_index += 1
+    return(l)
 
-arr = array.array('f', [99, 0, -7, 0, 16])
+temp_list = [23,44,0,-4123,312,0,222,0,321]
+#temp_list = [23,44,-4123,0,312,0,222,0,321]
+#temp_list = [23,44,-4123,312,222,0,0,321,0]
 
-for i in range(len(arr)):
-    print(f"{arr[i]:3}, {id(arr[i])}")
+temp_list_move_zeros = move_zeros(temp_list)
+print(temp_list)
+print(temp_list_move_zeros)
