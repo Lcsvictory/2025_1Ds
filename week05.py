f49879402a333ca4a7b80092acbbd9845d5e8c61
Lcsvictory <yuko135z@gmail.com>
2025-04-07 17:46:05 +0900

    stack with LinkedList

--- week05.py
+++ week05.py
@@ -1,33 +1,60 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
 class Stack:
     def __init__(self):
-        self.items = []
+        self.top = None
 
     def push(self, data):
-        self.items.append(data)
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.link = self.top
+            self.top = node
 
     def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        return len(self.items) == 0
+        if self.top is None:
+            return "stack is empty"
+        popped_node = self.top
+        self.top = self.top.link
+        popped_node.link = None
+        return popped_node.data
 
     def peek(self):
-        return self.items[-1]
+        if self.top is None:
+            return ""
+        print(self.top.data)
 
+    def size(self):
+        current = self.top
+        cnt = 0
+        if self.top is None:
+            return 0
+        while current:
+            cnt += 1
+            current = current.link
+        return cnt
+
+    def is_empty(self):
+        if self.top is None:
+            return True
+        return False
 
 s1 = Stack()
-s2 = Stack()
-print(s1.is_empty())
-s1.push("Ds")
-print(s1.is_empty())
-print(s2.is_empty())
-s1.push("DB")
+s1.push("data structure")
+s1.peek()
+s1.push("database")
+s1.push("database")
+
+s1.push("database")
+
+s1.push("databaseasdsd")
 print(s1.size())
-print(s1.peek())
-print(s1.size())
-print(s1.pop())
-print(s1.size())
-print(s1.peek())
+s1.peek()
+s1.pop()
+
+s1.pop()
+# s1.pop()
