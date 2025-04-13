fca1cdf4f5f1a10148a7f9c24aa96c71239f4a1f
Lcsvictory <yuko135z@gmail.com>
2025-04-13 23:37:45 +0900

    최소/최대값의 검색시간이 O(1)인 Stack

--- week05_practice2.py
+++ week05_practice2.py
@@ -1,28 +1,43 @@
+import random
 
 class MinStack:
     def __init__(self):
         self.main = []
         self.min = []
+        self.max = []
     
     def push(self, n):
         if len(self.main) == 0:
             self.min.append(n)
-        elif n <= self.min[-1]:
+            self.max.append(n)
+            self.main.append(n)
+            return
+        if n >= self.max[-1]:
+            self.max.append(n)
+        else:
+            self.max.append(self.max[-1])
+            
+        if n <= self.min[-1]:
             self.min.append(n)
         else:
             self.min.append(self.min[-1])
+
         self.main.append(n) 
 
     def pop(self):
-        if len(self.min) == 0: return
+        if len(self.main) == 0: return
         self.min.pop()
+        self.max.pop()
         return self.main.pop()
     
     def get_min(self):
         return self.min[-1]
     
+    def get_max(self):
+        return self.max[-1]
+    
     def __str__(self):
-        return f"main : {self.main}\nmin : {self.min}"
+        return f"main : {self.main}\nmin : {self.min}\nmax : {self.max}"
 
 def reverse_string(str):
     stack = list(str)
@@ -36,15 +51,16 @@ print(reverse_string("hello"))
 
 minstack = MinStack()
 
-minstack.push(20)
-minstack.push(2)
-minstack.push(6)
-minstack.push(40)
+for i in range(30):
+    minstack.push(random.randint(1,100))
+
 print(minstack)
 print(minstack.get_min())
+print(minstack.get_max())
+minstack.pop()
+minstack.pop()
 minstack.pop()
 print(minstack)
-minstack.pop()
-print(minstack)
-minstack.pop()
-print(minstack)
+print(minstack.get_min())
+print(minstack.get_max())
+
