55ca85492c9592e6c243f330cd1635737f449f47
Lcsvictory <yuko135z@gmail.com>
2025-04-14 17:31:33 +0900

    Queue

---
+++ week06.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+    def enqueue(self,data):
+        self.size += 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.link = node # 여기서 link는 진짜 rear가 아니네.
+            self.rear = node # 이게 진짜 rear. 따라서 rear.link는 무조건 None
+
+    def dequeue(self):
+        if self.front is None:
+            print("Queue is empty.")
+            # raise IndexError("queue is empty")
+            return
+        self.size -= 1
+        temp = self.front
+        self.front = self.front.link
+        temp.link = None
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+q = Queue()
+q.enqueue("ds")
+q.enqueue("DB")
+# q.enqueue("하이")
+print(q.size, q.front.data, q.rear.data, q.rear.link)
+
+print(q.dequeue())
+print(q.size, q.front.data, q.rear.data, q.rear.link)
+
