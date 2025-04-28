28f0bda04377a07a8a0a58db06c518f797c2fd10
Lcsvictory <yuko135z@gmail.com>
2025-04-28 18:11:19 +0900

    insert method add.

--- week08.py
+++ week08.py
@@ -25,6 +25,47 @@ def post_order(node):
 	print(node.data, end=" -> ")
 
 
+def find_number():
+	find_number = int(input("찾을 숫자를 입력 : "))
+	current = root
+	while True:
+		if find_number == current.data:
+			print(f"{find_number}을(를) 찾았습니다")
+			break
+		elif find_number < current.data:
+			if current.left is None:
+				print(f"{find_number}이(가) 존재하지 않습니다")
+				break
+			current = current.left
+		else:
+			if current.right is None:
+				print(f"{find_number}이(가) 존재하지 않습니다")
+				break
+			current = current.right
+
+#함수의 파라미터는 참조하지 않는가?
+def insert(root, *, value):
+	node = TreeNode()
+	node.data = value
+
+	if root is None:
+		return node
+
+	current = root
+
+	while True:
+		if node.data < current.data:
+			if current.left is None:
+				current.left = node
+				break
+			current = current.left
+		else:
+			if current.right is None:
+				current.right = node
+				break
+			current = current.right
+	return root
+
 class TreeNode:
 	def __init__(self):
 		self.left = None
@@ -34,30 +75,22 @@ class TreeNode:
 
 
 if __name__ == "__main__":
-	numbers = [10,15,8,3,9]
-	root = None
+	numbers = [10,15,8,3,9, 100, 7, 13]
 
-	node = TreeNode()
-	node.data = numbers[0]
-	root = node
+	#리턴값으로 참조를 계속 업데이트함. return값이 없으면 None을 반환하니
+	# root참조가 None으로 변경되버림
 
+	#root = None
+	#for number in numbers:
+		#root = insert(root=root, value=number)
+
+	# 파이썬은 참조니까.
+	root = TreeNode()
+	root.data = numbers[0]
 	for number in numbers[1:]:
-		node = TreeNode()
-		node.data = number
-		current = root
-		while True:
-			if node.data < current.data:
-				if current.left is None:
-					current.left = node
-					break
-				current = current.left
-			else:
-				if current.right is None:
-					current.right = node
-					break
-				current = current.right
+		insert(root=root, value=number)
 
 	print("binarySearchTree 구성완료")
-
+	#find_number()
 	post_order(root)
 
--- week08_practice.py
+++ week08_practice.py
@@ -1,3 +1,5 @@
+import random
+import time
 
 def in_order(node):
 	if node is None:
@@ -6,12 +8,15 @@ def in_order(node):
 	print(node.data, end=" -> ")
 	in_order(node.right)
 
+
+
 def pre_order(node):
 	if node is None:
 		return
 	print(node.data, end=" -> ")
 	pre_order(node.left)
 	pre_order(node.right)
+
 
 def post_order(node):
 	if node is None:
@@ -20,14 +25,39 @@ def post_order(node):
 	post_order(node.right)
 	print(node.data, end=" -> ")
 
+
+def find_number():
+	find_number = int(input("찾을 숫자를 입력 : "))
+	current = root
+	while True:
+		if find_number == current.data:
+			print(f"{find_number}을(를) 찾았습니다")
+			break
+		elif find_number < current.data:
+			if current.left is None:
+				print(f"{find_number}이(가) 존재하지 않습니다")
+				break
+			current = current.left
+		else:
+			if current.right is None:
+				print(f"{find_number}이(가) 존재하지 않습니다")
+				break
+			current = current.right
+
 class TreeNode:
 	def __init__(self):
 		self.left = None
 		self.data = None
 		self.right = None
 
+
+
 if __name__ == "__main__":
-	numbers = [10,15,8,3,9]
+	first = time.time()
+	print(first)
+	numbers = [random.choice(range(1,101)) for _ in range(100)]
+	#numbers = [i for i in range(1, 101)]
+	#numbers = [10,15,8,3,9]
 	root = None
 
 	node = TreeNode()
@@ -38,3 +68,22 @@ if __name__ == "__main__":
 		node = TreeNode()
 		node.data = number
 		current = root
+		while True:
+			if node.data < current.data:
+				if current.left is None:
+					current.left = node
+					break
+				current = current.left
+			else:
+				if current.right is None:
+					current.right = node
+					break
+				current = current.right
+
+	print("binarySearchTree 구성완료")
+	last = time.time()
+	print("소요시간 : " + str(last-first))
+	#find_number()
+	in_order(root)
+
+
