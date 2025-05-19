a4c66958147d5568de603bf5bd8cc02fb4f28097
Lcsvictory <yuko135z@gmail.com>
2025-05-19 21:15:45 +0900

    UPDATE bfs()

--- week11_practice.py
+++ week11_practice.py
@@ -17,21 +17,22 @@ def dfs(g, i, visited):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
 
-def bfs(g, i, visited, q):
-    visited[i] = True
-    print(chr(ord('A') + i), end=' ')
-    is_input = False
-    for j in range(len(graph)):
-        if g[i][j] == 1 and not visited[j]:
-            q.append(j)
-            is_input = True
-    if not is_input : return
-    bfs(g, q.popleft(), visited, q)
+def bfs(g, start):
+    visited = [False for _ in range(len(graph))]
+    visited[start] = True
+    q = deque()
+    q.append(start)
+    while q:
+        current = q.popleft()
+        print(chr(ord('A') + current), end=' ')
+        for j in range(len(graph)):
+            if g[current][j] == 1 and not visited[j]:
+                visited[j] = True
+                q.append(j)
 
 
 visited_dfs = [False for _ in range(len(graph))]
-visited_bfs = [False for _ in range(len(graph))]
 queue = deque()
 dfs(graph, 3, visited_dfs)
 print()
-bfs(graph, 3, visited_bfs, queue)
+bfs(graph, 4)
