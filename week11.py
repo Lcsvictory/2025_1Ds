382591523047fbc1c0ed7b1f7359af8419f0d28d
Lcsvictory <yuko135z@gmail.com>
2025-05-19 16:57:49 +0900

    UDPATE week11 add print_graph()

--- week11.py
+++ week11.py
@@ -1,10 +1,18 @@
 class Graph:
     def __init__(self, size):
-        self.SIZE = size
+        self.size = size
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
+
+    def print_graph(self):
+        for r in range(self.size):
+            for c in range(self.size):
+                print(self.graph[r][c], end=' ')
+            print()
 
 
 G1 = Graph(4)
+G2 = Graph(4)
+G_self = Graph(4)
 
 
 # 0 == A, 1 == B, 2 == C, 3 == D
@@ -14,8 +22,21 @@ G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
 G1.graph[3][0] = 1; G1.graph[3][2] = 1
 
 print("G1 무방향 그래프")
-for r in range(G1.SIZE):
-    for c in range(G1.SIZE):
-        print(G1.graph[r][c], end=' ')
-    print()
+G1.print_graph()
 
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G2 방향 그래프")
+G2.print_graph()
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G_self.graph[0][3] = 1
+G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
+G_self.graph[2][1] = 1
+G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
+
+print("G_self 무방향 그래프")
+G_self.print_graph()
