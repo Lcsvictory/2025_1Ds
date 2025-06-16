7212ead48d23ec7f9f347a372681fa97c1a27e7f
Lcsvictory <yuko135z@gmail.com>
2025-06-16 10:27:15 +0900

    UPDATE bfs

--- week11.py
+++ week11.py
@@ -1,12 +1,14 @@
+from collections import deque
 graph = [
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0]
+   # 0  1  2  3  4  5  6  7
+    [0, 1, 1, 0, 0, 0, 0, 0],  # A(0)
+    [1, 0, 0, 1, 0, 0, 0, 0],  # B(1)
+    [1, 0, 0, 1, 0, 0, 0, 0],  # C(2)
+    [0, 1, 1, 0, 1, 1, 1, 0],  # D(3)
+    [0, 0, 0, 1, 0, 1, 0, 0],  # E(4)
+    [0, 0, 0, 1, 1, 0, 0, 0],  # F(5)
+    [0, 0, 0, 1, 0, 0, 0, 1],  # G(6)
+    [0, 0, 0, 0, 0, 0, 1, 0]   # H(7)
 ]
 
 def dfs(g, i, visited):
@@ -17,7 +19,22 @@ def dfs(g, i, visited):
             dfs(g, j, visited)
 
 
+def bfs(graph, i, visited:list):
+    visited.append(i)
+    
+    q = deque()
+    q.append(i)
+    while q:
+        current = q.popleft()
+        print(chr(65 + current), end=" ")
+        for j in range(len(graph[current])):
+            if graph[current][j] == 1 and j not in visited:
+                q.append(j)
+                visited.append(j)
+
 
 visited_dfs = [False for _ in range(len(graph))]
-visited_bfs = [False for _ in range(len(graph))]
+visited_b = list()
 dfs(graph, 4, visited_dfs)
+print()
+bfs(graph, 4, visited_b)
